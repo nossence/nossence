f7d0df2c1d46ed7ea912759f2f58e17b623365d3
Joshua Zhang <toruta39@gmail.com>
2023-03-04 18:56:32 +0900

    use next export for static site generation

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' },
+    }
+  },
 }
 
 module.exports = nextConfig
